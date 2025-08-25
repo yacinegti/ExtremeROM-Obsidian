1048878afc7c9c43e0116e1337617dbeeb4f7f6e
Android-Artisan <romartisan2025@gmail.com>
2025-08-25 22:19:53 +0200

    target: init s20/n20 familly

---
+++ target/c1q/config.sh
@@ -0,0 +1,61 @@
+#
+# Copyright (C) 2025 BlackMesa123
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+# Device configuration file for Galaxy Note20 (snapdragon) (c1q)
+TARGET_NAME="Samsung Galaxy Note20 (snapdragon)"
+TARGET_CODENAME="c1q"
+TARGET_ASSERT_MODEL=("SM-N981N" "SM-N981U" "SM-N981W" "SM-N9810" "SM-N9818")
+TARGET_PLATFORM="sm8250"
+TARGET_FIRMWARE="SM-N981N/KOO/355347585240365"
+TARGET_EXTRA_FIRMWARES=("")
+TARGET_API_LEVEL=33
+TARGET_PRODUCT_FIRST_API_LEVEL=29
+TARGET_VNDK_VERSION=30
+TARGET_SINGLE_SYSTEM_IMAGE="qssi"
+TARGET_OS_FILE_SYSTEM="ext4"
+TARGET_SUPER_PARTITION_SIZE=0
+TARGET_SUPER_GROUP_NAME="group_basic"
+TARGET_SUPER_GROUP_SIZE=0
+TARGET_HAS_SYSTEM_EXT=false
+HAS_VENDOR=false
+TARGET_INSTALL_METHOD=odin
+TARGET_BOOT_DEVICE_PATH="/dev/block/by-name"
+
+TARGET_AUTO_BRIGHTNESS_TYPE="4"
+TARGET_DVFS_CONFIG_NAME="dvfs_policy_snapdragon865_qc"          
+TARGET_NFC_CHIP_VENDOR="SLSI"                                
+TARGET_FP_SENSOR_CONFIG="qualcomm_3d_sonic"                     
+TARGET_HAS_MASS_CAMERA_APP=false
+TARGET_HAS_QHD_DISPLAY=true       
+TARGET_HFR_MODE="2"
+TARGET_HFR_SUPPORTED_REFRESH_RATE="60,120"                
+TARGET_HFR_DEFAULT_REFRESH_RATE="120"
+TARGET_DISPLAY_CUTOUT_TYPE="center"
+TARGET_HFR_SEAMLESS_BRT="108,110"
+TARGET_HFR_SEAMLESS_LUX="200,2500"
+TARGET_IS_ESIM_SUPPORTED=false                                  
+TARGET_HAS_HW_MDNIE=true
+TARGET_MDNIE_SUPPORTED_MODES="65303"
+TARGET_MDNIE_WEAKNESS_SOLUTION_FUNCTION="3"
+TARGET_SUPPORT_WIFI_7=false                                      
+TARGET_SUPPORT_HOTSPOT_DUALAP=false
+TARGET_SUPPORT_HOTSPOT_WPA3=true                                
+TARGET_SUPPORT_HOTSPOT_6GHZ=false                            
+TARGET_SUPPORT_HOTSPOT_WIFI_6=true
+TARGET_SUPPORT_HOTSPOT_ENHANCED_OPEN=false
+TARGET_AUDIO_SUPPORT_ACH_RINGTONE=false
+TARGET_AUDIO_SUPPORT_VIRTUAL_VIBRATION=true
---
+++ target/y2q/sff.sh
@@ -0,0 +1,28 @@
+#
+# Copyright (C) 2023 Salvo Giangreco
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+# SEC Floating Feature configuration file for Galaxy S20+ (y2q)
+
+# Display
+SEC_FLOATING_FEATURE_LCD_SUPPORT_EXTRA_BRIGHTNESS=TRUE
+
+# Device
+SEC_FLOATING_FEATURE_SETTINGS_CONFIG_BRAND_NAME=Galaxy S20+
+
+# Misc
+SEC_FLOATING_FEATURE_GRAPHICS_SUPPORT_TOUCH_FAST_RESPONSE=
+SEC_FLOATING_FEATURE_SYSTEM_SUPPORT_LOW_HEAT_MODE=
