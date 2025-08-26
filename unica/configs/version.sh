5f09141efb264bce121864875ecf3426795cad5d
yacinegti <yacinecaddy2008@gmail.com>
2025-08-26 20:23:44 +0200

    Update version.sh

--- unica/configs/version.sh
+++ unica/configs/version.sh
@@ -23,4 +23,4 @@ ROM_VERSION="${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_PATCH}"
 # Append current commit hash to version name
 ROM_VERSION+="-$(git rev-parse --short HEAD)"
 
-ROM_CODENAME="Kebab"
+ROM_CODENAME="obsidian"
